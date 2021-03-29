17a97697e76ac76c494a6216d7bfce40c43ad087
Asineth <asineth@pm.me>
2021-03-29 10:18:10 -0400

    Added app download prompt to Home

---
+++ packages/client-web/src/components/AppDownloadModal.vue
@@ -0,0 +1,50 @@
+<template>
+  <Modal>
+    <div class="flex flex-col items-center p-8 space-y-8">
+      <p class="text-2xl font-bold">Download the app</p>
+      <div class="flex space-x-10 text-gray-400">
+        <a
+          href="/update/HyalusDesktop-win32.exe"
+          target="_blank"
+          ref="noreferrer noopener"
+        >
+          <WindowsIcon class="w-20 h-20 transition hover:text-gray-200" />
+        </a>
+        <a
+          href="/update/HyalusDesktop-osx.zip"
+          target="_blank"
+          ref="noreferrer noopener"
+        >
+          <AppleIcon class="w-20 h-20 transition hover:text-gray-200" />
+        </a>
+        <a
+          href="/update/HyalusDesktop-linux.AppImage"
+          target="_blank"
+          ref="noreferrer noopener"
+        >
+          <LinuxIcon class="w-20 h-20 transition hover:text-gray-200" />
+        </a>
+      </div>
+      <p class="px-4 py-2 bg-gray-900 border border-gray-800 rounded-md">
+        iOS &amp; Android apps coming soon.
+      </p>
+      <p
+        class="transition cursor-pointer text-primary-500 hover:text-primary-600"
+        @click="$emit('close')"
+      >
+        Close
+      </p>
+    </div>
+  </Modal>
+</template>
+
+<script>
+export default {
+  components: {
+    Modal: () => import("./Modal"),
+    WindowsIcon: () => import("../icons/Windows"),
+    AppleIcon: () => import("../icons/Apple"),
+    LinuxIcon: () => import("../icons/Linux"),
+  },
+};
+</script>
--- packages/client-web/src/views/Home.vue
+++ packages/client-web/src/views/Home.vue
@@ -6,27 +6,27 @@
       <h1 class="mt-8 text-6xl font-bold tracking-tight">
         Talk on an open platform
       </h1>
-      <p
-        class="max-w-lg mt-6 text-lg text-center text-gray-400"
-      >
+      <p class="max-w-lg mt-6 text-lg text-center text-gray-400">
         With cryptographically secure messages &amp; calls, talk on the platform
         that puts you in control.
       </p>
       <div class="flex mt-8 space-x-8">
-        <router-link
-          class="px-6 py-4 text-white transition rounded-lg shadow-sm bg-primary-500 hover:bg-primary-600"
+        <div
+          class="px-6 py-4 text-white transition rounded-lg shadow-sm cursor-pointer bg-primary-500 hover:bg-primary-600"
           to="/downloads"
-          >Download for Desktop</router-link
+          @click="appDownloadModal = true"
         >
+          Download for Desktop
+        </div>
         <router-link
-          class="block px-6 py-4 text-gray-400 transition rounded-lg bg-gray-800 hover:bg-gray-850"
+          class="block px-6 py-4 text-gray-400 transition bg-gray-800 rounded-lg hover:bg-gray-850"
           to="/app"
           >Open Web App</router-link
         >
       </div>
     </div>
     <div
-      class="flex flex-col items-center justify-center py-24 border-t border-b bg-gray-850 border-gray-800"
+      class="flex flex-col items-center justify-center py-24 border-t border-b border-gray-800 bg-gray-850"
     >
       <h1 class="text-4xl font-bold">A better way to communicate</h1>
       <p class="mt-4 text-lg text-gray-400">
@@ -36,7 +36,7 @@
       <div class="flex items-center w-full max-w-6xl mt-12 justify-evenly">
         <div class="flex flex-col max-w-xs">
           <LockIcon
-            class="w-12 h-12 p-3 rounded-full bg-gray-700 text-gray-400"
+            class="w-12 h-12 p-3 text-gray-400 bg-gray-700 rounded-full"
           />
           <h1 class="mt-4 text-2xl font-bold">Secure</h1>
           <p class="mt-2 text-gray-400">
@@ -47,7 +47,7 @@
         </div>
         <div class="flex flex-col max-w-xs">
           <ChipIcon
-            class="w-12 h-12 p-3 rounded-full bg-gray-700 text-gray-400"
+            class="w-12 h-12 p-3 text-gray-400 bg-gray-700 rounded-full"
           />
           <h1 class="mt-4 text-2xl font-bold">Open</h1>
           <p class="mt-2 text-gray-400">
@@ -58,7 +58,7 @@
         </div>
         <div class="flex flex-col max-w-xs">
           <PrivateIcon
-            class="w-12 h-12 p-3 rounded-full bg-gray-700 text-gray-400"
+            class="w-12 h-12 p-3 text-gray-400 bg-gray-700 rounded-full"
           />
           <h1 class="mt-4 text-2xl font-bold">Private</h1>
           <p class="mt-2 text-gray-400">
@@ -73,16 +73,25 @@
       <h1 class="z-10 text-2xl font-bold">
         "Cryptography is the ultimate form of non-violent direct action."
       </h1>
-      <p class="z-10 mt-4 text-md text-gray-400">
+      <p class="z-10 mt-4 text-gray-400 text-md">
         Julian Assange, founder of WikiLeaks
       </p>
     </div>
     <Footer />
+    <AppDownloadModal
+      v-if="appDownloadModal"
+      @close="appDownloadModal = false"
+    />
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      appDownloadModal: false,
+    };
+  },
   components: {
     LockIcon: () => import("../icons/Lock"),
     CodeIcon: () => import("../icons/Code"),
@@ -90,6 +99,7 @@ export default {
     ChipIcon: () => import("../icons/Chip"),
     Footer: () => import("../components/Footer"),
     Navbar: () => import("../components/Navbar"),
+    AppDownloadModal: () => import("../components/AppDownloadModal"),
   },
 };
 </script>
