0bc83401656f45061519c09c4428444ce26bc077
Asineth <asineth@pm.me>
2021-03-30 22:03:34 -0400

    client-web: disable outline on VoiceStream

--- packages/client-web/src/components/VoiceStream.vue
+++ packages/client-web/src/components/VoiceStream.vue
@@ -2,12 +2,14 @@
   <div>
     <p>{{ stream }}</p>
     <audio
+      class="outline-none"
       v-if="stream.type === 'audio' || stream.type === 'displayAudio'"
       controls="true"
       autoplay="true"
       :srcObject.prop="playerSrc"
     ></audio>
     <video
+      class="outline-none"
       v-if="stream.type === 'video' || stream.type === 'displayVideo'"
       controls="true"
       autoplay="true"
