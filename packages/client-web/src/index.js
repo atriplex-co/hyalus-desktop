4eece684f8c3e67c779a6954d18e1a9ca07c0c70
Asineth <asineth@pm.me>
2021-04-06 16:32:51 +0000

    client-web: disable productionTip

--- packages/client-web/src/index.js
+++ packages/client-web/src/index.js
@@ -6,6 +6,8 @@ import store from "./store";
 import router from "./router";
 import "./style.css";
 
+Vue.config.productionTip = false;
+
 console.log("%c[!] Console is for developers.", `color:#f55;`);
 console.log("%c[!] Pasting here may comprimise security!", `color:#f55;`);
 
