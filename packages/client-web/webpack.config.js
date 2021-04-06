eda1faeb1143c7faf7ff9c91a6b3ccd7317ced5b
Asineth <asineth@pm.me>
2021-04-06 12:33:55 +0000

    client-web: make all files follow contentHash

--- packages/client-web/webpack.config.js
+++ packages/client-web/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: path.join(__dirname, "src/index.js"),
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
+    filename: "[contenthash].js",
   },
   module: {
     rules: [
