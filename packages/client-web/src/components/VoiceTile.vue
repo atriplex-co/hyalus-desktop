a9927f39d0f8e7294e6e17751a081bb972485144
Asineth <asineth@pm.me>
2021-04-06 23:57:23 +0000

    client-web: add ability to set voice stream to fullscreen

--- packages/client-web/src/components/VoiceTile.vue
+++ packages/client-web/src/components/VoiceTile.vue
@@ -1,11 +1,12 @@
 <template>
-  <!-- <div class="w-full h-full flex items-center justify-center" v-else>
-        <UserAvatar class="w-32 h-32 rounded-full" :id="tile.user.avatar" />
-      </div> -->
-
-  <div class="relative rounded-md overflow-hidden border border-gray-700 shadow-lg">
-    <div class="aspect-w-16 aspect-h-9 bg-gray-850" />
-    <div class="absolute inset-0 flex items-center justify-center">
+  <div
+    class="relative rounded-md overflow-hidden border border-gray-700 shadow-lg"
+  >
+    <div class="aspect-w-16 aspect-h-9" />
+    <div
+      class="absolute inset-0 flex items-center justify-center bg-gray-900 group"
+      ref="main"
+    >
       <video
         class="w-full h-full"
         :srcObject.prop="srcObject"
@@ -25,6 +26,17 @@
           <p class="font-bold">{{ tile.user.name }}</p>
         </div>
       </div>
+      <div
+        class="absolute bottom-0 right-0 p-2 opacity-0 group-hover:opacity-100 transition"
+      >
+        <div
+          class="bg-gray-800 flex items-center space-x-2 p-2 rounded-md border-gray-750 border shadow-md text-gray-400"
+        >
+          <div @click="expand">
+            <ExpandIcon class="w-4 h-4 hover:text-gray-200 cursor-pointer" />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -32,15 +44,25 @@
 <script>
 export default {
   props: ["tile"],
-  computed: {
+  computed: { 
     srcObject() {
       if (this.tile.stream?.track.kind === "video") {
         return new MediaStream([this.tile.stream.track]);
       }
     },
   },
+  methods: {
+    expand() {
+      if (this.$refs.main !== document.fullscreenElement) {
+        this.$refs.main.requestFullscreen();
+      } else {
+        document.exitFullscreen();
+      }
+    },
+  },
   components: {
     UserAvatar: () => import("./UserAvatar"),
+    ExpandIcon: () => import("../icons/Expand"),
   },
 };
 </script>
