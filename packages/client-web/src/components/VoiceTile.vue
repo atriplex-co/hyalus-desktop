099c86fb4f0c074b5ee1ed4b23c782db50a64043
Asineth <asineth@pm.me>
2021-04-07 20:44:28 +0000

    client-web: fix VoiceTile corners

--- packages/client-web/src/components/VoiceTile.vue
+++ packages/client-web/src/components/VoiceTile.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex items-center justify-center group"
+    class="flex items-center justify-center group overflow-hidden"
     :class="{
       'bg-gray-900': srcObject,
       'bg-gray-850': !srcObject,
