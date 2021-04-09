4a092ee1a72b0b9744034398731404780b529c88
Asineth <asineth@pm.me>
2021-04-09 17:23:54 +0000

    client-web: fix z-indexes (for real this time)

--- packages/client-web/src/components/Modal.vue
+++ packages/client-web/src/components/Modal.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="z-20">
-    <div class="fixed inset-0 w-full h-full bg-black opacity-60" />
-    <div class="fixed inset-0 flex items-center justify-center w-full h-full">
+  <div>
+    <div class="fixed inset-0 w-full h-full bg-black opacity-60 z-20" />
+    <div class="fixed inset-0 flex items-center justify-center w-full h-full z-30">
       <div
         class="overflow-hidden border border-gray-800 rounded-md opacity-100 bg-gray-850 shadow-2xl"
       >
