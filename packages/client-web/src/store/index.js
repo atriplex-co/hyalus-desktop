b1f876e887f201a37fc40275e6561604aa902389
Asineth <asineth@pm.me>
2021-04-09 21:07:44 +0000

    client-web: scale down images above threshold

--- packages/client-web/src/store/index.js
+++ packages/client-web/src/store/index.js
@@ -1974,6 +1974,10 @@ export default new Vuex.Store({
       }
 
       if (imageTypes.find((t) => t === file.type)) {
+        //max width & height.
+        const mw = 2560;
+        const mh = 1440;
+
         fileType = "image/webp";
         fileName += ".webp";
 
@@ -1990,6 +1994,8 @@ export default new Vuex.Store({
           "libwebp",
           "-qscale",
           "80",
+          "-vf",
+          `scale='if(gt(a,${mw}/${mh}),${mw},-1)':'if(gt(a,${mw}/${mh}),-1,${mh})'`,
           fileName
         );
 
