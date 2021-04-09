43ba55bc3d14aa69715864fe7b874d08ef1cdac2
Asineth <asineth@pm.me>
2021-04-09 14:36:10 +0000

    client-web: move hover area for senderCard to UserAvatar

--- packages/client-web/src/components/ChannelMessage.vue
+++ packages/client-web/src/components/ChannelMessage.vue
@@ -15,12 +15,9 @@
         class="rounded-full"
         :id="sender.avatar"
         @mouseover.native="showSenderCard = true"
+        @mouseleave.native="showSenderCard = false"
       />
-      <div
-        class="absolute pb-10 bottom-0 w-72"
-        v-if="showSenderCard"
-        @mouseleave="showSenderCard = false"
-      >
+      <div class="absolute pb-10 bottom-0 w-72" v-if="showSenderCard">
         <div
           class="bg-gray-800 rounded-md p-4 border border-gray-750 flex items-center space-x-4"
         >
