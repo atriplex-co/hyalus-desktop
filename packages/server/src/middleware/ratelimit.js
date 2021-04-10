a9cfbad13745354977d51b46b4425d0a53d6eba2
Asineth <asineth@pm.me>
2021-04-10 07:48:31 +0000

    server: remove ratelimit debug code

--- packages/server/src/middleware/ratelimit.js
+++ packages/server/src/middleware/ratelimit.js
@@ -1,58 +1,52 @@
-module.exports = (config) => {
-  console.log(JSON.stringify(config));
+module.exports = (config) => async (req, res, next) => {
+  let key = `ratelimit`;
 
-  return async (req, res, next) => {
-    let key = `ratelimit`;
+  if (config.tag) {
+    key += `:${config.tag}`;
+  }
 
-    if (config.tag) {
-      key += `:${config.tag}`;
+  if (config.scope === "ip") {
+    key += `:${req.ip}`;
+  }
+
+  if (config.scope === "user") {
+    key += `:${req.session.user.toString()}`;
+  }
+
+  if (config.params) {
+    key += `:${Object.values(req.params).join()}`;
+  }
+
+  const status = await req.deps.redis.get(key);
+
+  if (!status) {
+    await req.deps.redis.set(
+      key,
+      {
+        start: Date.now(),
+        left: config.max - 1,
+      },
+      "ex",
+      config.time
+    );
+  } else {
+    if (!status.left) {
+      return res.status(429).json({
+        error: "Too many requests",
+        expires: status.start + config.time,
+      });
     }
 
-    if (config.scope === "ip") {
-      key += `:${req.ip}`;
-    }
+    await req.deps.redis.set(
+      key,
+      {
+        start: Date.now(),
+        left: status.left - 1,
+      },
+      "ex",
+      config.time - Math.floor((Date.now() - status.start) / 1000)
+    );
+  }
 
-    if (config.scope === "user") {
-      key += `:${req.session.user.toString()}`;
-    }
-
-    if (config.params) {
-      key += `:${Object.values(req.params).join()}`;
-    }
-
-    const status = await req.deps.redis.get(key);
-
-    if (!status) {
-      await req.deps.redis.set(
-        key,
-        {
-          start: Date.now(),
-          left: config.max - 1,
-        },
-        "ex",
-        config.time
-      );
-    } else {
-      if (!status.left) {
-        return res.status(429).json({
-          error: "Too many requests",
-          expires: status.start + config.time,
-        });
-      }
-
-      console.log({ config, status });
-
-      await req.deps.redis.set(
-        key,
-        {
-          start: Date.now(),
-          left: status.left - 1,
-        },
-        "ex",
-        config.time - Math.floor((Date.now() - status.start) / 1000)
-      );
-    }
-
-    next();
-  };
+  next();
 };
