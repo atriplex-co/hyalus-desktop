2851a997860c3c6c13381fd98fd074962b21b568
Asineth <asineth@pm.me>
2021-04-13 04:13:30 +0000

    client-web: divide ChannelMessages by date

--- packages/client-web/src/components/ChannelMessage.vue
+++ packages/client-web/src/components/ChannelMessage.vue
@@ -1,121 +1,126 @@
 <template>
-  <div
-    class="max-w-lg mx-auto text-sm text-gray-200"
-    v-if="message.event"
-    :class="{
-      'pb-4': precedingMessage && precedingMessage.event,
-      'py-4': !precedingMessage || !precedingMessage.event,
-    }"
-  >
-    {{ message.event }}
-  </div>
-  <div class="flex items-end space-x-2" v-else>
-    <div class="w-8 h-8 relative" v-if="lastFromSender">
-      <UserAvatar
-        class="rounded-full"
-        :id="sender.avatar"
-        @mouseover.native="showSenderCard = true"
-        @mouseleave.native="showSenderCard = false"
-      />
-      <div
-        class="absolute bottom-10 w-72 bg-gray-800 rounded-md p-4 border border-gray-750 flex items-center space-x-4"
-        v-if="showSenderCard"
-      >
-        <UserAvatar class="w-12 h-12 rounded-full" :id="sender.avatar" />
-        <div>
-          <p class="font-bold truncate">{{ sender.name }}</p>
-          <p class="text-xs text-gray-400">@{{ sender.username }}</p>
-        </div>
-      </div>
+  <div>
+    <div class="text-center text-sm text-gray-400 py-6" v-if="showSection">
+      {{ time }}
     </div>
-    <div class="p-4" v-else />
-    <div class="flex items-center space-x-4 group">
-      <div
-        class="max-w-xs lg:max-w-md xl:max-w-2xl rounded-md text-sm overflow-hidden"
-        :class="{
-          'bg-gradient-to-br from-primary-500 to-primary-600':
-            sentByMe && !message.fileMediaType,
-          'bg-gray-800': !sentByMe && !message.fileMediaType,
-        }"
-      >
-        <div class="p-2" v-if="message.type === 'text'">
-          <div class="break-words whitespace-pre-wrap" v-html="body" />
-        </div>
+    <div
+      class="max-w-lg mx-auto text-sm text-gray-200"
+      v-if="message.event"
+      :class="{
+        'pb-4': precedingMessage && precedingMessage.event,
+        'py-4': !precedingMessage || !precedingMessage.event,
+      }"
+    >
+      {{ message.event }}
+    </div>
+    <div class="flex items-end space-x-2" v-else>
+      <div class="w-8 h-8 relative" v-if="lastFromSender">
+        <UserAvatar
+          class="rounded-full"
+          :id="sender.avatar"
+          @mouseover.native="showSenderCard = true"
+          @mouseleave.native="showSenderCard = false"
+        />
         <div
-          class="p-2"
-          v-if="message.type === 'file' && !message.fileMediaType"
+          class="absolute bottom-10 w-72 bg-gray-800 rounded-md p-4 border border-gray-750 flex items-center space-x-4"
+          v-if="showSenderCard"
         >
-          <div class="flex items-center space-x-2 py-1">
-            <div @click="saveFile">
-              <DownloadIcon
-                class="w-8 h-8 p-2 rounded-full bg-primary-400 text-white cursor-pointer"
-              />
-            </div>
-            <div>
-              <p class="truncate">{{ message.decryptedFileName }}</p>
-              <p
-                class="text-xs"
-                :class="{
-                  'text-primary-200': sentByMe,
-                  'text-gray-400': !sentByMe,
-                }"
-              >
-                {{ fileLength }}
-              </p>
-            </div>
+          <UserAvatar class="w-12 h-12 rounded-full" :id="sender.avatar" />
+          <div>
+            <p class="font-bold truncate">{{ sender.name }}</p>
+            <p class="text-xs text-gray-400">@{{ sender.username }}</p>
           </div>
         </div>
-        <div v-if="message.fileMediaType && message.blob">
-          <img
-            :src="message.blob"
-            v-if="message.fileMediaType === 'img'"
-            class="cursor-pointer"
-            @click="showImageView = true"
-          />
-          <audio
-            :src="message.blob"
-            v-if="message.fileMediaType === 'audio'"
-            controls
-            class="outline-none"
-          />
-          <video
-            :src="message.blob"
-            v-if="message.fileMediaType === 'video'"
-            controls
-            class="outline-none"
-          />
+      </div>
+      <div class="p-4" v-else />
+      <div class="flex items-center space-x-4 group">
+        <div
+          class="max-w-xs lg:max-w-md xl:max-w-2xl rounded-md text-sm overflow-hidden"
+          :class="{
+            'bg-gradient-to-br from-primary-500 to-primary-600':
+              sentByMe && !message.fileMediaType,
+            'bg-gray-800': !sentByMe && !message.fileMediaType,
+          }"
+        >
+          <div class="p-2" v-if="message.type === 'text'">
+            <div class="break-words whitespace-pre-wrap" v-html="body" />
+          </div>
+          <div
+            class="p-2"
+            v-if="message.type === 'file' && !message.fileMediaType"
+          >
+            <div class="flex items-center space-x-2 py-1">
+              <div @click="saveFile">
+                <DownloadIcon
+                  class="w-8 h-8 p-2 rounded-full bg-primary-400 text-white cursor-pointer"
+                />
+              </div>
+              <div>
+                <p class="truncate">{{ message.decryptedFileName }}</p>
+                <p
+                  class="text-xs"
+                  :class="{
+                    'text-primary-200': sentByMe,
+                    'text-gray-400': !sentByMe,
+                  }"
+                >
+                  {{ fileLength }}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div v-if="message.fileMediaType && message.blob">
+            <img
+              :src="message.blob"
+              v-if="message.fileMediaType === 'img'"
+              class="cursor-pointer"
+              @click="showImageView = true"
+            />
+            <audio
+              :src="message.blob"
+              v-if="message.fileMediaType === 'audio'"
+              controls
+              class="outline-none"
+            />
+            <video
+              :src="message.blob"
+              v-if="message.fileMediaType === 'video'"
+              controls
+              class="outline-none"
+            />
+          </div>
+          <div v-if="message.fileMediaType && !message.blob">
+            <LoadingIcon class="w-10 h-10 p-2" />
+          </div>
         </div>
-        <div v-if="message.fileMediaType && !message.blob">
-          <LoadingIcon class="w-10 h-10 p-2" />
+        <div class="flex items-center space-x-3">
+          <div
+            class="text-gray-400 transition opacity-0 cursor-pointer group-hover:opacity-100 hover:text-gray-200"
+            v-if="sentByMe"
+            @click="remove"
+          >
+            <TrashIcon class="w-5 h-5" />
+          </div>
+          <div
+            class="text-gray-400 transition opacity-0 cursor-pointer group-hover:opacity-100 hover:text-gray-200"
+            v-if="message.fileMediaType && message.blob"
+            @click="saveFile"
+          >
+            <DownloadIcon class="w-5 h-5" />
+          </div>
+          <p
+            class="opacity-0 group-hover:opacity-100 text-xs text-gray-400 transition"
+          >
+            {{ time }}
+          </p>
         </div>
       </div>
-      <div class="flex items-center space-x-3">
-        <div
-          class="text-gray-400 transition opacity-0 cursor-pointer group-hover:opacity-100 hover:text-gray-200"
-          v-if="sentByMe"
-          @click="remove"
-        >
-          <TrashIcon class="w-5 h-5" />
-        </div>
-        <div
-          class="text-gray-400 transition opacity-0 cursor-pointer group-hover:opacity-100 hover:text-gray-200"
-          v-if="message.fileMediaType && message.blob"
-          @click="saveFile"
-        >
-          <DownloadIcon class="w-5 h-5" />
-        </div>
-        <p
-          class="opacity-0 group-hover:opacity-100 text-xs text-gray-400 transition"
-        >
-          {{ time }}
-        </p>
-      </div>
+      <ImageView
+        :image="message.blob"
+        v-if="showImageView"
+        @close="showImageView = false"
+      />
     </div>
-    <ImageView
-      :image="message.blob"
-      v-if="showImageView"
-      @close="showImageView = false"
-    />
   </div>
 </template>
 
@@ -191,6 +196,13 @@ export default {
       });
 
       return `${Math.round(len * 10) / 10} ${unit}`;
+    },
+    showSection() {
+      return (
+        !this.precedingMessage ||
+        moment(this.precedingMessage.time).day() !==
+          moment(this.message.time).day()
+      );
     },
   },
   methods: {
