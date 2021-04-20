375c44f6732cc97b6a42d4410e3959510cbd11ff
Asineth <asineth@pm.me>
2021-04-20 16:23:59 -0400

    client-web: add tip cards

--- packages/client-web/src/components/AppDownloadModal.vue
+++ packages/client-web/src/components/AppDownloadModal.vue
@@ -1,8 +1,8 @@
 <template>
   <Modal>
-    <div class="flex flex-col items-center p-8 space-y-8">
+    <div class="flex flex-col items-center p-8">
       <p class="text-2xl font-bold">Download the app</p>
-      <div class="flex space-x-10 text-gray-400">
+      <div class="mt-8 flex space-x-10 text-gray-400">
         <a
           href="https://github.com/hyalusapp/hyalus/releases/latest/download/HyalusDesktop-win32.exe"
           target="_blank"
@@ -25,11 +25,11 @@
           <LinuxIcon class="w-20 h-20 transition hover:text-gray-200" />
         </a>
       </div>
-      <p class="px-4 py-2 bg-gray-900 border border-gray-800 rounded-md">
+      <p class="mt-8 px-4 py-2 bg-gray-900 border border-gray-800 rounded-md">
         iOS &amp; Android apps coming soon.
       </p>
       <p
-        class="transition cursor-pointer text-primary-500 hover:text-primary-600"
+        class="transition cursor-pointer text-primary-500 hover:text-primary-600 mt-6"
         @click="$emit('close')"
       >
         Close
