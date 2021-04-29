2e24a306b9e03d929794ecece704aa3ce2b9eee9
Asineth <asineth@pm.me>
2021-04-28 23:25:38 -0400

    client-web: fix useravatar semicircles

--- packages/client-web/src/components/UserAvatar.vue
+++ packages/client-web/src/components/UserAvatar.vue
@@ -1,29 +1,39 @@
 <template>
   <div v-if="id" class="overflow-hidden">
-    <img
-      :src="`${baseUrl}/api/avatars/${id}`"
-      @load="showImg = true"
-      @error="showImg = false"
-      :class="{
-        hidden: !showImg,
-      }"
-    />
-    <video :src="`${baseUrl}/api/avatars/${id}`" autoplay loop muted />
+    <img :src="url" v-if="type === 'image'" />
+    <video :src="url" autoplay loop muted v-if="type === 'video'" />
   </div>
   <img src="../images/default-user.png" v-else />
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   props: ["id"],
   data() {
     return {
-      showImg: false,
+      type: null,
+      url: null,
     };
   },
-  computed: {
-    baseUrl() {
-      return this.$store.getters.baseUrl;
+  methods: {
+    async update() {
+      axios.defaults.baseURL = this.$store.getters.baseUrl;
+      const { data, headers } = await axios.get(`/api/avatars/${this.id}`, {
+        responseType: "blob",
+      });
+
+      this.type = headers["content-type"].split("/")[0];
+      this.url = URL.createObjectURL(data);
+    },
+  },
+  created() {
+    this.update();
+  },
+  watch: {
+    id() {
+      this.update();
     },
   },
   components: {
