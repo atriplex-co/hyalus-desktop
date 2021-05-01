77c6f81600b15d58bfc0a71f1a58b2c6ae9c157e
Asineth <asineth@pm.me>
2021-05-01 19:47:39 +0000

    client-web: add favicon for SEO

--- packages/client-web/src/views/Login.vue
+++ packages/client-web/src/views/Login.vue
@@ -23,6 +23,7 @@
                 class="w-full px-4 py-2 text-gray-400 bg-gray-900 border rounded-sm border-gray-750 focus:outline-none focus:border-gray-650"
                 type="text"
                 v-model="username"
+                autocomplete="username"
               />
             </div>
             <div class="space-y-2">
