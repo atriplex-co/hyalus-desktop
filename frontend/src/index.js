d1f8ead675d3d49827b45dafcd52346e0d27b050
Asineth <asineth@pm.me>
2021-08-25 02:52:37 +0000

    frontend: move css into App.vue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import store from "./store";
 import router from "./router";
 import { ObserveVisibility } from "vue-observe-visibility";
-import "./index.css";
 
 window.store = store;
 
