d3b311b7e1f8b1bad15548673e6b963bc0984bed
Asineth <asineth@pm.me>
2021-09-05 01:11:12 +0000

    frontend: add GroupCreateModal to SidebarChannelList

--- frontend/src/components/SidebarChannelList.vue
+++ frontend/src/components/SidebarChannelList.vue
@@ -1,37 +1,76 @@
 <template>
   <!-- why w-screen? literally no fucking idea. -->
   <div class="max-w-xs w-screen bg-gray-700 flex flex-col">
-    <div class="h-16 flex items-center px-4 text-gray-200 text-2xl font-bold">
+    <div
+      class="
+        h-16
+        flex
+        items-center
+        px-4
+        text-gray-200 text-2xl
+        font-bold
+        justify-between
+      "
+    >
       <p v-if="type === 'private'">Direct Messages</p>
-      <p v-if="type === 'group'">Groups</p>
+      <template v-if="type === 'group'">
+        <p>Groups</p>
+        <div @click="groupCreateModal = true">
+          <PlusIcon
+            class="
+              w-8
+              h-8
+              p-2
+              text-white
+              rounded-full
+              bg-primary-500
+              cursor-pointer
+              hover:bg-primary-600
+              transition
+            "
+          />
+        </div>
+      </template>
     </div>
     <div class="flex-1 overflow-auto divide-y divide-gray-600">
-      <div></div>
+      <div />
       <SidebarChannel
         v-for="channel in channels"
         :key="channel.id"
         :channel="channel"
       />
-      <div></div>
+      <div />
     </div>
+    <GroupCreateModal
+      v-if="groupCreateModal"
+      @close="groupCreateModal = false"
+    />
   </div>
 </template>
 
 <script setup>
 import SidebarChannel from "./SidebarChannel.vue";
-import { computed, defineProps, watch } from "vue";
+import GroupCreateModal from "./GroupCreateModal.vue";
+import PlusIcon from "../icons/Plus.vue";
+import { computed, defineProps, ref, watch } from "vue";
 import { useStore } from "vuex";
 import { useRoute, useRouter } from "vue-router";
 
 const store = useStore();
+
 const route = useRoute();
+
 const router = useRouter();
+
 const props = defineProps({
   type: {
     type: String,
     default: "",
   },
 });
+
+const groupCreateModal = ref(false);
+
 const channels = computed(() =>
   store.getters.channels
     .filter((c) => c.type === props.type)
