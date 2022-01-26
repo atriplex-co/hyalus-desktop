d1dae99a04048b6dc2e3347cf65bfa86a276caf9
Asineth <asineth@pm.me>
2022-01-26 18:12:56 -0500

    root: fix docker-compose on win32

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 export DOCKER_BUILDKIT=1
 export COMPOSE_DOCKER_CLI_BUILD=1
+rm -rf ~/.docker
 docker-compose -f docker-compose.dev.yml up -d --build
--- scripts/prod.sh
+++ scripts/prod.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 export DOCKER_BUILDKIT=1
 export COMPOSE_DOCKER_CLI_BUILD=1
+rm -rf ~/.docker
 docker-compose up -d --build
