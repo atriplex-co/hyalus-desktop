c013162329b57e53408da25f2f6fc6138f20aa23
Asineth <asineth@pm.me>
2023-09-30 15:55:37 -0400

    use DMG for macOS distribution & add notarization support

---
+++ scripts/afterSign.js
@@ -0,0 +1,27 @@
+const path = require("node:path");
+const { notarize } = require("@electron/notarize");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+module.exports = async (context) => {
+  if (context.electronPlatformName === "darwin") {
+    console.log("notarizing macOS app");
+    
+    let appBundleId = "";
+    if (context.packager.appInfo.productName === "Hyalus") {
+      appBundleId = "app.hyalus";
+    }
+    if (context.packager.appInfo.productName === "Hyalus Staging") {
+      appBundleId = "app.hyalus.staging";
+    }
+
+    await notarize({
+      appBundleId,
+      appPath: path.join(context.appOutDir, `${context.packager.appInfo.productName}.app`),
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_ID_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID,
+    });
+  }
+};
